f97d92289151f896bcc423c08c8be93d2d9c86bf
Jakub <Wilk>
2022-12-09 09:40:29 -0600

    Initial Commit

---
+++ tictactoeapp/tictactoeappApp.swift
@@ -0,0 +1,17 @@
+//
+//  tictactoeappApp.swift
+//  tictactoeapp
+//
+//  Created by Jakub Wilk on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct tictactoeappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
